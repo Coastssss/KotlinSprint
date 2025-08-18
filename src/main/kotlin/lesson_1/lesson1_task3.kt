9bb1822cf9fc8def0d7340463bdf5d8a35113535
bltks (Svetlana) <mysterious.coast@bk.ru>
2025-08-18 20:56:45 +0300

    KS-1-3 output flight time to the console

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main()
+{
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int =7
+
+    println("Взлет:")
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    println("Посадка: $hour:$minute")
+}
