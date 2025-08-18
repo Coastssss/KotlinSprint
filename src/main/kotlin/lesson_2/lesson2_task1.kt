39ceb8a606dcbc4fc836a13b8f6f56327fbf1cf1
bltks (Svetlana) <mysterious.coast@bk.ru>
2025-08-18 22:24:26 +0300

    KS-1-5 format arithmetic mean

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+fun main() {
+    val sr1 = 3
+    val sr2 = 4
+    val sr3 = 5
+    val sr4 = 3
+    val srAr: Double = (sr1+sr2+sr3+sr4).toDouble()/4
+
+    println("%.2f".format(srAr))
+}
