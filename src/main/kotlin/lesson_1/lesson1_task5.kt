ffefa3e48780e24da3c96f144f4574c542719d7f
bltks (Svetlana) <mysterious.coast@bk.ru>
2025-08-19 18:29:47 +0300

    KS-1-5 fix

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,10 +1,12 @@
 package org.example.lesson_1
 
 fun main() {
+    val secondsInMinute = 60
+    val secondsInHour = 3600
     var seconds = 6480
-    val minutes = (seconds % 3600) / 60
-    val hour = seconds / 3600
+    val minutes = (seconds % secondsInHour) / secondsInMinute
+    val hours = seconds / secondsInHour
 
-    seconds = seconds - hour*3600 - minutes*60
-    println("Время полета: %02d:%02d:%02d".format(hour,minutes,seconds))
+    seconds = seconds - hours * secondsInHour - minutes * secondsInMinute
+    println("Время полета: %02d:%02d:%02d".format(hours, minutes, seconds))
 }
