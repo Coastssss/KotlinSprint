f3f34660aa2d57fd29e0c88faf688ff3117e59de
bltks (Svetlana) <mysterious.coast@bk.ru>
2025-08-20 18:30:58 +0300

    KS-3-1 greeting

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_3
+
+fun main() {
+    var greeting: String = "Добрый день"
+    val userName: String = "Светлана"
+
+    println("$greeting, $userName!")
+    greeting = "Добрый вечер"
+    println("$greeting, $userName!")
+}
