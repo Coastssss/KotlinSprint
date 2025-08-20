4612725cc5558fc9902e87aae49f33538888849d
bltks (Svetlana) <mysterious.coast@bk.ru>
2025-08-20 17:08:26 +0300

    KS-1-5 rename permanent names

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_1
 
-fun main() {
-    val secondsInMinute = 60
-    val secondsInHour = 3600
-    var seconds = 6480
-    val minutes = (seconds % secondsInHour) / secondsInMinute
-    val hours = seconds / secondsInHour
+const val SECONDS_IN_MINUTE = 60
+const val SECONDS_IN_HOUR = 3600
 
-    seconds = seconds - hours * secondsInHour - minutes * secondsInMinute
+fun main() {
+    var seconds = 6480
+    val minutes = (seconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val hours = seconds / SECONDS_IN_HOUR
+
+    seconds = seconds - hours * SECONDS_IN_HOUR - minutes * SECONDS_IN_MINUTE
     println("Время полета: %02d:%02d:%02d".format(hours, minutes, seconds))
 }
