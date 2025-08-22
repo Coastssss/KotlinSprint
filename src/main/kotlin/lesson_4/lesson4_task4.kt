0f0c3fefc42043e8fdc324ad145cbd1c716e6f61
bltks (Svetlana) <mysterious.coast@bk.ru>
2025-08-22 19:35:23 +0300

    KS-4-3 simplification

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,12 +2,10 @@ package org.example.lesson_4
 
 fun main() {
     val trainingDay = 5
-
-    val isOddDay = (trainingDay % 2) != 0
     val isEvenDay = (trainingDay % 2) == 0
 
-    println("Упражнения для рук:    $isOddDay\n" +
+    println("Упражнения для рук:    ${!isEvenDay}\n" +
             "Упражнения для ног:    $isEvenDay\n" +
             "Упражнения для спины:  $isEvenDay\n" +
-            "Упражнения для пресса: $isOddDay")
+            "Упражнения для пресса: ${!isEvenDay}")
 }
