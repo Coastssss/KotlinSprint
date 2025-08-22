ab37bb9deedc0be87ba10bb48decf18efa044c44
bltks (Svetlana) <mysterious.coast@bk.ru>
2025-08-22 18:31:38 +0300

    KS-4-3 fix const

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,12 +1,14 @@
 package org.example.lesson_4
 
+const val REQUIRED_HUMIDITY = 20
+const val PROHIBITED_SEASON = "зима"
 fun main() {
     val isSunnyWeather = true
     val isTentOpen = true
     val humidity = 20
     val currentSeason = "зима"
 
-    val isFavorableConditions: Boolean = isSunnyWeather && isTentOpen && humidity == 20 && currentSeason != "зима"
+    val isFavorableConditions: Boolean = isSunnyWeather && isTentOpen && humidity == REQUIRED_HUMIDITY && currentSeason != PROHIBITED_SEASON
 
     println("Благоприятные ли условия сейчас для роста бобовых? $isFavorableConditions")
 }
