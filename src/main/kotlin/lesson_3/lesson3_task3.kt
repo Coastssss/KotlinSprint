e761ecb4281190741f5c6f5c3f0b3b7cc2ac26ba
bltks (Svetlana) <mysterious.coast@bk.ru>
2025-08-22 19:39:31 +0300

    KS-3-3 fix

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -3,5 +3,13 @@ package org.example.lesson_3
 fun main() {
     val number = 6
 
-    for (i in 1..9) println("$number x $i = ${i * number}")
+    println("$number x 1 = ${1 * number}\n" +
+            "$number x 2 = ${2 * number}\n" +
+            "$number x 3 = ${3 * number}\n" +
+            "$number x 4 = ${4 * number}\n" +
+            "$number x 5 = ${5 * number}\n" +
+            "$number x 6 = ${6 * number}\n" +
+            "$number x 7 = ${7 * number}\n" +
+            "$number x 8 = ${8 * number}\n" +
+            "$number x 9 = ${9 * number}")
 }
